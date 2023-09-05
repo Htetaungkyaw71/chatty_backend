e9cb7481fff68491914890af398a12e38bcd6008
Htetaungkyaw71 <htetaung200071@gmail.com>
2023-09-05 09:57:11 +0630

    fix last error

--- src/handlers/contact.ts
+++ src/handlers/contact.ts
@@ -1,22 +1,27 @@
 import prisma from "../db";
 
-export const getAllContact = async(req,res)=>{
-    const user = await prisma.user.findUnique({
-        where:{
-            id:req.user.id
+export const getAllContact = async (req, res) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          id: req.user.id,
         },
-        include:{
-            contacts:true
-        }
-    })
-    if(!user){
-        res.status(404)
-        res.json({messgae:"user is not found"})
+        include: {
+          contacts: true,
+        },
+      });
+  
+      if (!user) {
+        res.status(404).json({ message: "User not found" });
         return;
+      }
+  
+      res.json({ data: user.contacts });
+    } catch (error) {
+      console.error("Error in getAllContact:", error);
+      res.status(500).json({ message: "Internal server error" });
     }
-    res.json({data:user.contacts})
-}
-
+  };
 export const createContact = async(req,res)=>{
     try {
         const existingContact = await prisma.contact.findFirst({
@@ -26,7 +31,7 @@ export const createContact = async(req,res)=>{
             }
         });
 
-        console.log("existing",existingContact)
+      
     
         if (existingContact) {
             res.status(409).json({ message: 'Contact already exists' });
@@ -50,10 +55,16 @@ export const createContact = async(req,res)=>{
 }
 
 export const deleteContact = async (req,res) => {
-    const contact = await prisma.contact.delete({
-        where:{
-            id:req.params.id
-        }
-    })
-    res.json({data:contact})
+    try {
+        const contact = await prisma.contact.delete({
+            where:{
+                id:req.params.id
+            }
+        })
+        res.json({data:contact})
+    } catch (error) {
+        console.error("Error in deleteContact:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+   
 }
